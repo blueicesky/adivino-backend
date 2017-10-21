d699a98aefaf97b4112dc615d5e67b1ad6c0f7f0
Pillip Lee <phillip0711@hotmail.com>
2017-10-21 15:10:51 -0400

    Script for getting additional player stats

---
+++ api/matchTime.py
@@ -0,0 +1,54 @@
+from time import sleep, time
+from datetime import datetime
+import pandas as pd
+import json
+
+match_file = 'EPL_Fixture_1718.csv'		# name of the csv file
+player_file = 'fifa_player.csv'
+
+def find_profile(player_name):
+	df_p = pd.read_csv(player_file)
+	game_time = ""
+
+	for index, rows in df_p.iterrows():
+		if (rows['Name'] == player_name):
+			age = df_p.iloc[index]['Age']
+			photo = df_p.iloc[index]['Photo']
+			nation = df_p.iloc[index]['Nationality']
+			pos = df_p.iloc[index]['Preferred Positions']
+			break;
+	return age, photo, nation, pos
+
+def find_matchInfo(team_name):
+	df_m = pd.read_csv(match_file)
+	for index, rows in df_m.iterrows():
+		game_date = datetime.strptime(df_m.iloc[index]['DATE'], '%Y-%m-%d')
+		#print(datetime.strptime((str(datetime.now()))[:10], '%Y-%m-%d'))
+		if (game_date > datetime.now()):
+			if ((rows['HOME TEAM'] == team_name) or (rows['AWAY TEAM'] == team_name)):
+				game_date = df_m.iloc[index]['DATE']
+				game_hour = df_m.iloc[index]['TIME']
+				fixture = df_m.iloc[index]['FIXTURE']
+				game_time = (game_date + " " + game_hour)
+				game_time = datetime.strptime(game_time, '%Y-%m-%d %H:%M')
+				break;
+	return game_time
+
+def main():
+	with open('player.json') as data_file:  
+		data = json.load(data_file)
+	
+	club_name = 'Chelsea'
+	player = 'Rooney'
+
+	match_info = find_matchInfo(club_name)
+	age, photo, nation, pos = find_profile(player)
+
+	print('The next game is at ' + str(match_info))
+	print('Age: ' + str(age))
+	print('Photo: ' + photo)
+	print('Nation: ' + nation)
+	print('Pos: ' + pos)
+
+if __name__ == "__main__":
+	main()
